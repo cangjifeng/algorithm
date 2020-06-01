89f9f25d111e4577e5d1405898a67ff4440b3e25
cangjifeng <cangjifeng@139.com>
2020-06-01 17:40:01 +0800

    AlgorithmApplication 工程搭建

---
+++ src/main/java/org/jerfan/algorithm/AlgorithmApplication.java
@@ -0,0 +1,17 @@
+package org.jerfan.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmApplication {
+
+    /**
+     * 访问 http://localhost:8013/start
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class, args);
+    }
+
+}
